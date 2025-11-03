5d7d8e2900f6d76e68c582e4e67811f291661d4f
Mateusz <mateusz.kolodziejczyk04@gmail.com>
2025-11-04 00:01:52 +0100

    Added simple Spring Security with db authentication

--- src/main/java/com/liftmetrics/security/SecurityConfig.java
+++ src/main/java/com/liftmetrics/security/SecurityConfig.java
@@ -1,26 +1,39 @@
 package com.liftmetrics.security;
+import com.liftmetrics.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private MyUserDetailsService myUserDetailsService;
+
+    public SecurityConfig(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // wyłączenie CSRF (żeby POST z Postmana działał bez tokena)
                 .csrf(AbstractHttpConfigurer::disable)
-                // konfiguracja uprawnień dla endpointów
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/exercise/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        daoAuthenticationProvider.setUserDetailsService(myUserDetailsService);
+        return daoAuthenticationProvider;
     }
 }
---
+++ src/main/java/com/liftmetrics/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.liftmetrics.service;
+
+import com.liftmetrics.model.User;
+import com.liftmetrics.model.UserPrincipal;
+import com.liftmetrics.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user==null){
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new UserPrincipal(user);
+    }
+}
